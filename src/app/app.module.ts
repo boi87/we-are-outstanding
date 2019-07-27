9dd2e872ece58c9da24c533df65dcea0231f7db0
Michele Boi <boimik87@gmail.com>
2019-07-28 00:36:33 +0100

    material installed, npm audit fix not run

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { AddReviewComponent } from "./add-review/add-review.component";
-import { ReviewsComponent } from "./reviews/reviews.component";
-import { AddReviewService } from "./add-review/addReview.service";
-import { MatButtonModule, MatCheckboxModule } from "@angular/material";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AddReviewComponent } from './add-review/add-review.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { AddReviewService } from './add-review/addReview.service';
+// import { MatButtonModule, MatCheckboxModule } from "@angular/material";
 import { ReviewDetailComponent } from './reviews/review-detail/review-detail.component';
 
 @NgModule({
